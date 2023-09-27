4947c8bbf8f3e53e84a9de2120e4d582b4f88191
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-09-27 12:40:07 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,18 +1,17 @@
 # Parte 1
 
 def sum(array)
-  array.reduce(0, :+)
+
 end
 
 
 def max_2_sum(array)
-  sorted_array = array.sort.reverse
-  sum_of_max_2 = sorted_array.take(2).sum
+
 end
 
 
 def sum_to_n?(array, n)
-  array.combination(2).any? { |a, b| a + b == n }
+
 end
  
 
